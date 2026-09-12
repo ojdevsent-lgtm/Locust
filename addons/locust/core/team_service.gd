450f7c1ad951a34b9ea66e4be5006536bcf97db4
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-12 19:22:31 +0400

    Fix team service role normalization

--- addons/locust/core/team_service.gd
+++ addons/locust/core/team_service.gd
@@ -9,11 +9,15 @@ func normalize_collaborators(data):
 	if typeof(data) != TYPE_ARRAY:
 		return result
 	for item in data:
-		result.append({
-			"login": str(item.get("login", "")),
-			"avatar": str(item.get("avatar_url", "")),
-			"role": str(item.get("permissions", {}).get("admin", false) if item.get("permissions", {}).get("admin", false) else "collaborator")
-		})
+		var permissions = item.get("permissions", {})
+		var role = "collaborator"
+		if permissions.get("admin", false):
+			role = "admin"
+		elif permissions.get("push", false):
+			role = "write"
+		elif permissions.get("pull", false):
+			role = "read"
+		result.append({"login": str(item.get("login", "")), "avatar": str(item.get("avatar_url", "")), "role": role})
 	return result
 
 func normalize_commits(data):
@@ -23,12 +27,7 @@ func normalize_commits(data):
 	for item in data:
 		var commit = item.get("commit", {})
 		var author = commit.get("author", {})
-		result.append({
-			"sha": str(item.get("sha", "")),
-			"message": str(commit.get("message", "")).split("\n")[0],
-			"author": str(author.get("name", "")),
-			"date": str(author.get("date", ""))
-		})
+		result.append({"sha": str(item.get("sha", "")), "message": str(commit.get("message", "")).split("\n")[0], "author": str(author.get("name", "")), "date": str(author.get("date", ""))})
 	return result
 
 func normalize_pull_requests(data):
@@ -36,11 +35,5 @@ func normalize_pull_requests(data):
 	if typeof(data) != TYPE_ARRAY:
 		return result
 	for item in data:
-		result.append({
-			"number": int(item.get("number", 0)),
-			"title": str(item.get("title", "")),
-			"author": str(item.get("user", {}).get("login", "")),
-			"state": str(item.get("state", "")),
-			"url": str(item.get("html_url", ""))
-		})
+		result.append({"number": int(item.get("number", 0)), "title": str(item.get("title", "")), "author": str(item.get("user", {}).get("login", "")), "state": str(item.get("state", "")), "url": str(item.get("html_url", ""))})
 	return result
