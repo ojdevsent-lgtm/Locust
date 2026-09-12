04d4aae83a3ba387f1f40ab54ef24a99167db022
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-12 19:07:19 +0400

    Fix snapshot hashes to match Git blobs

--- addons/locust/core/snapshot.gd
+++ addons/locust/core/snapshot.gd
@@ -15,24 +15,28 @@ func load_snapshot():
 		return {}
 	return parsed.result
 
-func save_snapshot(snapshot):
+func save_snapshot(data):
 	var dir = Directory.new()
 	if not dir.dir_exists("res://.locust"):
 		dir.make_dir_recursive("res://.locust")
 	var file = File.new()
 	if file.open(SNAPSHOT_PATH, File.WRITE) != OK:
 		return false
-	file.store_string(JSON.print(snapshot, "\t"))
+	file.store_string(JSON.print(data, "\t"))
 	file.close()
 	return true
 
-func checksum(data):
-	return Marshalls.raw_to_base64(data.sha256_buffer())
+func git_blob_sha(data):
+	var header = "blob " + str(data.size()) + "\u0000"
+	var bytes = PoolByteArray()
+	bytes.append_array(header.to_utf8())
+	bytes.append_array(data)
+	return bytes.sha1_text()
 
 func build(files, scanner):
 	var result = {}
 	for path in files:
 		var data = scanner.read_file(path)
 		if data != null:
-			result[path] = checksum(data)
+			result[path] = git_blob_sha(data)
 	return result
